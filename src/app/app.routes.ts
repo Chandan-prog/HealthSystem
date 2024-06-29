0373c997323a07648ce4f3232d82a8da2f37be0a
Chandan-prog <mishrachandan1997.cm@gmail.com>
2024-06-29 15:03:38 +0530

    Home page changes and creating doctor & patient comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth/auth.guard';
+import { DoctorComponent } from './home/doctor/doctor.component';
+import { PatientComponent } from './home/patient/patient.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +14,13 @@ export const routes: Routes = [
         path: 'home',
         component: HomeComponent,
         canActivate: [AuthGuard],
-    }
+    },
+    {
+        path: 'doctor',
+        component: DoctorComponent
+    },
+    {
+        path: 'patient',
+        component: PatientComponent
+    },
 ];
